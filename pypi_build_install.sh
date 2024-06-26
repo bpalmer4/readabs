9d88d7552e0e0f622080b7da6447e89b43f2d9e7
bpalmer4 <palmer.bryan@gmail.com>
2024-06-26 11:19:14 +1000

    Uploaded Version 0.0.2 to PyPI

--- pypi_build_install.sh
+++ pypi_build_install.sh
@@ -1,5 +1,13 @@
+#!/bin/zsh
 # make sure the version number is correct in setup.py
 
-python setup.py sdist
+pip uninstall readabs
+
+pip install -Ue .
+
+pip install -q build
+
+python -m build
 
 twine upload dist/*
+
